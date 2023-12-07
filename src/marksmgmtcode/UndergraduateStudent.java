7b52f268fe415408f137bb4b43ce48ff91e6db1a
peravali <peravali@pdx.edu>
2023-12-06 18:19:16 -0800

    Changed under gradutate student variable References for major along with its Code Refactoring

--- src/marksmgmtcode/UndergraduateStudent.java
+++ src/marksmgmtcode/UndergraduateStudent.java
@@ -1,23 +1,23 @@
 package marksmgmtcode;
 
 public class UndergraduateStudent extends Student {
-	private String major;
+	private String fieldOfStudy;
 
-	public UndergraduateStudent(String id, String name, String major) {
-		super(id, name);
-		this.major = major;
+	public UndergraduateStudent(String studentId, String studentName, String fieldOfStudy) {
+		super(studentId, studentName);
+		this.fieldOfStudy = fieldOfStudy;
 	}
 
-	public String getMajor() {
-		return major;
+	public String getFieldOfStudy() {
+		return fieldOfStudy;
 	}
 
-	public void setMajor(String major) {
-		this.major = major;
+	public void setFieldOfStudy(String fieldOfStudy) {
+		this.fieldOfStudy = fieldOfStudy;
 	}
 
 	@Override
 	public String toString() {
-		return super.toString() + ", Major: " + major;
+		return super.toString() + ", fieldOfStudy: " + fieldOfStudy;
 	}
 }
